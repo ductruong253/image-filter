37b3fdb3969fe38a2d45085cc907297cb3138b93
ductruong253 <ddtruong94@gmail.com>
2022-07-21 22:10:59 +0700

    complete feature after local test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import { ExecException } from 'child_process';
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalSingleFile, isUrlValid } from './util/util';
 
 (async () => {
 
@@ -9,19 +10,43 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  app.get("/filteredimage", async (req, res) => {
+    // IT SHOULD
+    //    1. validate the image_url query
+    let url = (req.query as any).image_url;
+    if (!isUrlValid(url)) return res.status(400).send("URL is missing or invalid");
+    //Returning file
+    try {
+      //    2. call filterImageFromURL(image_url) to filter the image
+      const processedImagePath = await filterImageFromURL(url);
+      //    3. send the resulting file in the response
+      res.sendFile(processedImagePath, () => {
+        //    4. deletes any files on the server on finish of the response
+        deleteLocalSingleFile(processedImagePath);
+      })
+    }
+    catch (e: any) {
+      console.log("Error occured while processing image")
+      return res.status(500).send({
+        "message": "Error occured while processing image"
+      })
+    }
+
+
+    console.log("Ending process...")
+    return res;
+  })
+
+
+
+
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
@@ -30,17 +55,17 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
-
+const URL = require("url").URL;
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -12,8 +12,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
@@ -21,8 +20,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
         .write(__dirname + outpath, (img) => {
           resolve(__dirname + outpath);
         });
-    } catch (error) {
-      reject(error);
+    } catch (e) {
+      reject(`Invalid image; ${e.message}`);
     }
   });
 }
@@ -35,5 +34,32 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
+  }
+}
+
+// deleteLocalSingleFile
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalSingleFile(file: string) {
+  console.log(`Deleting file: ${file}`)
+  fs.unlinkSync(file);
+}
+
+//isUrlValid
+//Check if the URL is valid
+//INPUT:
+//    check: string, input string for validation
+//OUTPUT:
+//    true on valid URL, otherwise false
+export function isUrlValid(check: string) {
+  try {
+    new URL(check);
+    return true;
+  }
+  catch {
+    console.error(`Error: URL seem invalid: ${check}`);
+    return false;
   }
 }
